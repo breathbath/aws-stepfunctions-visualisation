872e811e6152ca69cf6bb25596ae17661aa3afd1
Andrey P <posandrey@gmail.com>
2019-04-18 15:54:15 +0200

    Added start end states and recursive parallel states listing

--- src/pages/Graph.js
+++ src/pages/Graph.js
@@ -11,6 +11,15 @@ import { CardContent } from '@material-ui/core';
 import { fetchAwsWithErrorHandling } from '../components/AwsFetcher';
 
 class Graph extends React.Component {
+  dGraphFormat = 'digraph  { %defaultStyles %nodeStyles %edges}';
+  dDefaultStylesFormat = 'node [shape="box"]';
+  startEndNodeStyleFormat = '%node [shape="circle", id="%id", fillcolor="%color", style="filled"]';
+  dDefaultNodeStyleFormat = '%node [shape="box", URL="%url", id="%id"]';
+  dFailedNodeStyleFormat = '%node [shape="box", URL="%url", id="%id", style=dashed]';
+  dFilledNodeStyleFormat = '%node [fillcolor="%color", shape="box", URL="%url", style="filled", id="%id"]';
+  dEdgeFormat = '%node1 -> %node2';
+  dDottedEdgeFormat = '%node1 -> %node2[style=dashed, color=red]';
+
   constructor(props) {
     super(props);
     this.classes = props.classes;
@@ -72,7 +81,7 @@ class Graph extends React.Component {
   }
 
   handleClick(e) {
-    if (!e.target || !e.target.parentNode || e.target.parentNode.nodeName != "a") {
+    if (!e.target || !e.target.parentNode || e.target.parentNode.nodeName !== "a") {
       return;
     }
     let currentStateName = e.target.parentNode.getAttribute("title");
@@ -82,6 +91,51 @@ class Graph extends React.Component {
     }
     e.preventDefault();
     this.stateCard.current.setCurrentSmState(currentStateObj);
+  }
+
+  buildStatesGraph(states, nodeStyles, edges, startStateName, endStateName, entryPointStateName) {
+    edges.push(this.dEdgeFormat.replace("%node1", startStateName).replace("%node2", entryPointStateName));
+
+    let state = {};
+    for (var stateId in states) {
+      if (!states.hasOwnProperty(stateId)) {
+        continue;
+      }
+      state = states[stateId];
+      this.stateHistoryMap[stateId] = {};
+      let nodeStyleFormat = this.dDefaultNodeStyleFormat;
+      if (state.Next && state.Type !== "Parallel") {
+        edges.push(this.dEdgeFormat.replace("%node1", stateId).replace("%node2", state.Next));
+      }
+      if (state.Catch && state.Catch.length > 0) {
+        nodeStyleFormat = this.dFailedNodeStyleFormat;
+        for (let k = 0; k < state.Catch.length; k++) {
+          let curCatch = state.Catch[k];
+          if (!curCatch.Next) {
+            continue;
+          }
+          edges.push(this.dDottedEdgeFormat.replace("%node1", stateId).replace("%node2", curCatch.Next));
+        }
+      }
+
+      if (state.Type === "Succeed" || state.End) {
+        edges.push(this.dEdgeFormat.replace("%node1", stateId).replace("%node2", endStateName));
+      } else if (state.Type === "Fail") {
+        nodeStyleFormat = this.dFailedNodeStyleFormat;
+        edges.push(this.dDottedEdgeFormat.replace("%node1", stateId).replace("%node2", endStateName));
+      } else if (state.Type === "Parallel") {
+        for (var branchStateIndex in state.Branches) {
+          let subStateMachine = state.Branches[branchStateIndex];
+          this.buildStatesGraph(subStateMachine.States, nodeStyles, edges, stateId, state.Next, subStateMachine.StartAt);
+        }
+      }
+
+      nodeStyles.push(
+        nodeStyleFormat.replace("%node", stateId)
+        .replace("%url", "/sm/" + this.stateMachineArn + "/e/" + this.executionArn + "/state/" + stateId)
+        .replace("%id", stateId)
+      );
+    }
   }
 
   render() {
@@ -108,50 +162,29 @@ class Graph extends React.Component {
       return (<div>{comment}</div>);
     }
 
-    let dGraphFormat = 'digraph  { %defaultStyles %nodeStyles %edges}';
-    let dDefaultStylesFormat = 'node [shape="box"]';
-    let dDefaultNodeStyleFormat = '%node [shape="box", URL="%url", id="%id"]';
-    let dFilledNodeStyleFormat = '%node [fillcolor="%color", shape="box", URL="%url", style="filled", id="%id"]';
-    let dEdgeFormat = '%node1 -> %node2';
-    let dDottedEdgeFormat = '%node1 -> %node2[style=dashed, color=red]';
-
-    let dGraph = dGraphFormat.replace("%defaultStyles", dDefaultStylesFormat);
+    let dGraph = this.dGraphFormat.replace("%defaultStyles", this.dDefaultStylesFormat);
     let nodeStyles = [];
     let edges = [];
 
-    let state = "";
-    for (var stateId in stateMachineDefinition.States) {
-      if (!stateMachineDefinition.States.hasOwnProperty(stateId)) {
-        continue;
-      }
-      state = stateMachineDefinition.States[stateId];
-      this.stateHistoryMap[stateId] = {};
+    nodeStyles.push(
+      this.startEndNodeStyleFormat.replace("%node", 'Start')
+      .replace("%color", 'orange')
+      .replace("%id", 'Start')
+    );
 
-      nodeStyles.push(
-        dDefaultNodeStyleFormat.replace("%node", stateId)
-        .replace("%url", "/sm/" + this.stateMachineArn + "/e/" + this.executionArn + "/state/" + stateId)
-        .replace("%id", stateId)
-      );
-      if (state.Next) {
-        edges.push(dEdgeFormat.replace("%node1", stateId).replace("%node2", state.Next));
-      }
-      if (state.Catch && state.Catch.length > 0) {
-        for (let k = 0; k < state.Catch.length; k++) {
-          let curCatch = state.Catch[k];
-          if (!curCatch.Next) {
-            continue;
-          }
-          edges.push(dDottedEdgeFormat.replace("%node1", stateId).replace("%node2", curCatch.Next));
-        }
-      }
-    }
+    nodeStyles.push(
+      this.startEndNodeStyleFormat.replace("%node", 'End')
+      .replace("%color", 'orange')
+      .replace("%id", 'End')
+    );
+    this.buildStatesGraph(stateMachineDefinition.States, nodeStyles, edges, 'Start', 'End', stateMachineDefinition.StartAt)
 
     let lastExitedStateName = '';
     for (let k = 0; k < historyEvents.length; k++) {
       let historyEvent = historyEvents[k];
       if (historyEvent.type.endsWith("StateEntered") && historyEvent.stateEnteredEventDetails.name && this.stateHistoryMap[historyEvent.stateEnteredEventDetails.name]) {
         let stateUrl = "/sm/" + this.stateMachineArn + "/e/" + this.executionArn + "/state/" + historyEvent.stateEnteredEventDetails.name;
-        nodeStyles.push(dFilledNodeStyleFormat.replace("%node", historyEvent.stateEnteredEventDetails.name)
+        nodeStyles.push(this.dFilledNodeStyleFormat.replace("%node", historyEvent.stateEnteredEventDetails.name)
         .replace("%color", "green")
         .replace("%url", stateUrl)
         .replace("%id", historyEvent.stateEnteredEventDetails.name)
@@ -165,7 +198,7 @@ class Graph extends React.Component {
       }
 
       if (historyEvent.type === "ExecutionFailed" && lastExitedStateName) {
-        nodeStyles.push(dFilledNodeStyleFormat.replace("%node", lastExitedStateName).replace("%color", "red"));
+        nodeStyles.push(this.dFilledNodeStyleFormat.replace("%node", lastExitedStateName).replace("%color", "red"));
         lastExitedStateName = "";
         continue;
       }
@@ -181,7 +214,7 @@ class Graph extends React.Component {
         <Grid item xs>
           <Card>
             <CardContent>
-              <div onClick={this.handleClick}>{comment}<Graphviz dot={dGraph} /></div>
+              <div onClick={this.handleClick}>{comment}<Graphviz dot={dGraph} options={{fit:true, width:"600", height:"700"}} /></div>
             </CardContent>
           </Card>
         </Grid>
